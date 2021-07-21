e1f967de24a7184bf326cfcf1f51fc3f93be4f16
Ben Grewell <BGrewell@gmail.com>
2021-07-21 10:50:52 -0700

    add escape quotes

--- utilities.go
+++ utilities.go
@@ -8,6 +8,7 @@ import (
 // Fields is a special function similar in nature to the standard libraries strings.Fields except that
 // it honors quoted strings so it will not split inside of quoted values in a string.
 func Fields(s string) (fields []string, err error) {
+	s = strings.ReplaceAll(s,"\"", "\\\"")
 	r := csv.NewReader(strings.NewReader(s))
 	r.Comma = ' '
 	return r.Read()
