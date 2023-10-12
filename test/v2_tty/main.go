24711733d2a823796e38a9d9c7a125d1528ebeac
Ben Grewell <bgrewell@gmail.com>
2023-10-12 06:00:27 -0700

    add error handling

--- test/v2_tty/main.go
+++ test/v2_tty/main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"fmt"
 	v2 "github.com/BGrewell/go-execute/v2"
 )
 
 func main() {
 	e := v2.NewExecutorAsUser("whoopsie", []string{"BOB=YOUR_UNCLE"})
-	e.ExecuteTTY("/bin/bash")
+	err := e.ExecuteTTY("/bin/bash")
+	if err != nil {
+		fmt.Printf("failed to execute: %v\n", err)
+	}
 }
