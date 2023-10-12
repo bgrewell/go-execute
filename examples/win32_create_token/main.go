934f2841c5fac0cfad9eb276917f8c860e061ab5
Ben Grewell <bgrewell@gmail.com>
2023-10-12 09:00:16 -0700

    add test for getting windows tokens

---
+++ examples/win32_create_token/main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"fmt"
+	"syscall"
+	"unsafe"
+)
+
+var (
+	modntdll               = syscall.NewLazyDLL("ntdll.dll")
+	procNtCreateToken      = modntdll.NewProc("NtCreateToken")
+	procRtlAdjustPrivilege = modntdll.NewProc("RtlAdjustPrivilege")
+)
+
+func NtCreateTokenAsSid(sid *syscall.SID) (syscall.Handle, error) {
+	// This is a placeholder structure. You'll need to define the actual TOKEN_USER structure
+	var tokenUser struct {
+		User syscall.SIDAndAttributes
+	}
+	tokenUser.User.Attributes = 0 // typically 0
+	tokenUser.User.Sid = sid
+
+	// Placeholder values; these need to be correctly defined
+	var tokenHandle syscall.Handle
+	var objectAttributes uintptr
+	var tokenType int32 // TokenPrimary typically
+	var authId syscall.LUID
+	var expirationTime int64
+	var tokenGroups, tokenPrivileges, tokenOwner, tokenPrimaryGroup, tokenDefaultDacl, tokenSource uintptr
+
+	status, _, _ := procNtCreateToken.Call(
+		uintptr(unsafe.Pointer(&tokenHandle)),
+		// DesiredAccess, typically TOKEN_ALL_ACCESS
+		uintptr(0xF01FF),
+		objectAttributes,
+		tokenType,
+		uintptr(unsafe.Pointer(&authId)),
+		uintptr(unsafe.Pointer(&expirationTime)),
+		uintptr(unsafe.Pointer(&tokenUser)),
+		tokenGroups,
+		tokenPrivileges,
+		tokenOwner,
+		tokenPrimaryGroup,
+		tokenDefaultDacl,
+		tokenSource,
+	)
+	if status != 0 {
+		return 0, syscall.NTStatus(status)
+	}
+
+	return tokenHandle, nil
+}
+
+func main() {
+	// Example usage
+	sid, _ := syscall.StringToSid("S-1-5-21-...") // put actual SID string here
+	token, err := NtCreateTokenAsSid(sid)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	defer syscall.CloseHandle(token)
+
+	fmt.Println("Token created:", token)
+}
