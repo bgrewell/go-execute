51ec09072d2fff9c4da6fdd86e4fd22b9f37eeec
Ben Grewell <bgrewell@gmail.com>
2023-11-13 08:31:00 -0800

    refactor: change input to a bufio.Reader

--- helpers.go
+++ helpers.go
@@ -3,20 +3,16 @@ package execute
 import (
 	"bufio"
 	"errors"
-	"io"
 	"time"
 )
 
 // SignalRead checks if the reader is ready to be read from before the timeout.
-func SignalRead(reader io.ReadCloser, timeout time.Duration) (ready bool, err error) {
-	// Channels to signal when data is ready or an error occurred
+func SignalRead(bufferedReader *bufio.Reader, timeout time.Duration) (ready bool, err error) {
 	readyChan := make(chan bool)
 	errChan := make(chan error)
 
-	// Goroutine to perform the reading
 	go func() {
-		bufferedReader := bufio.NewReader(reader)
-		_, err := bufferedReader.Peek(1) // Just peeking the first byte
+		_, err := bufferedReader.Peek(1)
 		if err != nil {
 			if err != bufio.ErrBufferFull {
 				errChan <- err
@@ -26,16 +22,12 @@ func SignalRead(reader io.ReadCloser, timeout time.Duration) (ready bool, err er
 		readyChan <- true
 	}()
 
-	// Select to wait on channels or timeout
 	select {
 	case <-readyChan:
-		// Data is ready
 		return true, nil
 	case err := <-errChan:
-		// An error occurred while reading
 		return false, err
 	case <-time.After(timeout):
-		// Timeout occurred
 		return false, errors.New("read timeout")
 	}
 }
