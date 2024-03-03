7f9091bf32223879401ad1c9aaa4b62a99ff5e3c
Ben Grewell <BGrewell@gmail.com>
2024-03-03 15:27:23 -0800

    Update exe.go

--- v2/exe.go
+++ v2/exe.go
@@ -9,11 +9,11 @@ import (
 type Executor interface {
 	Execute(command string) (combined string, err error)
 	ExecuteSeparate(command string) (stdout string, stderr string, err error)
-	ExecuteAsync(command string) (*ExecutionResult, err error)
-	ExecuteAsyncWithInput(command string, stdin io.ReadCloser) (*ExecutionResult, err error)
+	ExecuteAsync(command string) (result *ExecutionResult, err error)
+	ExecuteAsyncWithInput(command string, stdin io.ReadCloser) (result *ExecutionResult, err error)
 	ExecuteWithTimeout(command string, timeout time.Duration) (combined string, err error)
 	ExecuteSeparateWithTimeout(command string, timeout time.Duration) (stdout string, stderr string, err error)
-	ExecuteAsyncWithTimeout(command string, timeout time.Duration) (*ExecutionResult, err error)
+	ExecuteAsyncWithTimeout(command string, timeout time.Duration) (result *ExecutionResult, err error)
 	ExecuteTTY(command string) error
 }
 
